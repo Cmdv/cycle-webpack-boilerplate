6c396861ae765aa749eb25fc086d34cbc8bd17c2
Vincent Orr <Vincent@cmdv.org>
2015-12-04 19:25:26 +0000

    trying again with click test

--- src/__test__/navbar.spec.js
+++ src/__test__/navbar.spec.js
@@ -4,10 +4,29 @@ import createRenderTarget   from './helper/createRenderTarget';
 import {makeHistoryDriver}  from '@cycle/history';
 import Navbar               from '../dialogue/navbar';
 import Cycle                from '@cycle/core';
+import {filterLinks} from '@cycle/history'
+import {getUrl, extractValue, events} from '../utils/utils'
 import {h,makeDOMDriver,mockDOMResponse} from '@cycle/dom';
 
 test('Navbar tests', function(t) {
   t.plan(3);
+
+  const intent = ({DOM}) => ({
+    click$: events(
+      DOM.select('.link'), [
+        `click`,
+        `touchstart`,
+      ])
+      .filter(filterLinks),
+  })
+
+  const model = ({click$}, {History}) => {
+    return latestObj({
+      url: click$
+        .map(getUrl)
+        .startWith(History.value.pathname),
+    })
+  }
 
   // mock the Main component and pass it the responses
   function mainComponent(responses) {
@@ -15,10 +34,13 @@ test('Navbar tests', function(t) {
     return requests
   }
 
+  const actions = intent(responses)
+  const state$ = model(actions, responses)
+
   // Pass Main through our RenderTarget
   let {sinks, sources} = Cycle.run(mainComponent, {
     DOM: makeDOMDriver(createRenderTarget()),
-    url$: '/'
+    url$: extractValue(`url`, state$)
   });
 
   // Uncaught TypeError: drivers[_name2] is not a function
